3e6bc15b815779c9bcafe84537736361acab6379
andreyliporage <andreyliporage@gmail.com>
2020-11-14 11:26:04 -0300

    criando controller para sessao

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
@@ -43,7 +44,7 @@ public class SessaoController {
 		return modelAndView;
 	}
 	
-	@PostMapping(value = "/admin/sessao")
+	@RequestMapping(value = "/admin/sessao")
 	@Transactional
 	public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
 		if (result.hasErrors()) return form (form.getSalaId(), form);
